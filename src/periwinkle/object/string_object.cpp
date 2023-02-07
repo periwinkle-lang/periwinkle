4fc26b12e4be29e921bd13b3e7d945e9e3ca1ab7
romanfedyniak <fedynuak.roma@gmail.com>
2023-02-07 17:41:50 +0200

    Зміна логіки конкатенації для StringObject

--- src/periwinkle/object/string_object.cpp
+++ src/periwinkle/object/string_object.cpp
@@ -1,20 +1,37 @@
 ﻿#include "string_object.h"
 #include "native_function_object.h"
 #include "exception_object.h"
+#include "null_object.h"
 
 using namespace vm;
 
-#define CHECK_STRING(object)                             \
-    if (object->objectType->type != ObjectTypes::STRING) \
-       return &P_NotImplemented;
-
-static Object* strAdd(Object* a, Object* b)
+static bool tryConvertToString(Object * o, std::string& str)
 {
-    CHECK_STRING(a);
-    CHECK_STRING(b);
-    auto str1 = (StringObject*)a;
-    auto str2 = (StringObject*)b;
-    return StringObject::create(str1->value + str2->value);
+    if (o->objectType->operators->toString != NULL)
+    {
+        str = ((StringObject*)Object::toString(o))->value;
+        return true;
+    }
+    return false;
+}
+
+// Конвертує об'єкт до StringObject, окрім випадку, коли об'єкт типу "нич"
+#define TO_STRING(object, str)                           \
+    if (object->objectType->type == ObjectTypes::STRING) \
+        str = ((StringObject*)object)->value;            \
+    else                                                 \
+    {                                                    \
+        if (object == &P_null                            \
+            || tryConvertToString(object, str) == false) \
+            return &P_NotImplemented;                    \
+    }
+
+static Object* strAdd(Object* o1, Object* o2)
+{
+    std::string a, b;
+    TO_STRING(o1, a);
+    TO_STRING(o2, b);
+    return StringObject::create(a + b);
 }
 
 Object* allocStringObject();
