4ad42b3153ab093148afcc2ffdde52e41fdcb161
romanfedyniak <fedynuak.roma@gmail.com>
2023-02-09 20:14:15 +0200

    Перевірка на правильність консольних аргументів

--- src/periwinkle/main.cpp
+++ src/periwinkle/main.cpp
@@ -2,30 +2,35 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+
 #include "periwinkle.h"
+
+using std::string;
+using std::vector;
+using std::wstring;
 
 #ifdef _WIN32
     #include "windows.h"
 
-std::wstring convertUtf8ToWide(const std::string& str)
+wstring convertUtf8ToWide(const string& str)
 {
     int count = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.length(), NULL, 0);
-    std::wstring wstr(count, 0);
+    wstring wstr(count, 0);
     MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.length(), &wstr[0], count);
     return wstr;
 }
 
-std::string convertWideToUtf8(const std::wstring& wstr)
+string convertWideToUtf8(const wstring& wstr)
 {
     int count = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), NULL, 0, NULL, NULL);
-    std::string str(count, 0);
+    string str(count, 0);
     WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
     return str;
 }
 #endif
 
 
-std::string readCode(std::string filename)
+string readCode(string filename)
 {
 #ifdef _WIN32
     std::ifstream filestream(convertUtf8ToWide(filename));
@@ -47,7 +52,7 @@ std::string readCode(std::string filename)
     }
 }
 
-std::string usage(std::string programName)
+string usage(string programName)
 {
     std::stringstream ss;
     ss << "Барвінок " << periwinkle::Periwinkle::getVersionAsString() << std::endl;
@@ -61,51 +66,19 @@ std::string usage(std::string programName)
     return ss.str();
 }
 
-std::string getCmdOption(std::vector<std::string> tokens, std::string option, std::string fullOption = "")
+bool cmdOptionExists(vector<string> tokens, string option, string fullOption = "")
 {
-    auto itr = std::find(tokens.begin() + 1, tokens.end(), option);
-    if (itr != tokens.end() && ++itr != tokens.end())
-    {
-        return *itr;
-    }
-
-    if (fullOption != "")
-    {
-        auto fullItr = std::find(tokens.begin() + 1, tokens.end(), fullOption);
-        if (fullItr != tokens.end() && ++fullItr != tokens.end())
-        {
-            return *fullItr;
-        }
-    }
-
-    std::cerr << "Опція \"" << fullOption << "\" відсутня!" << std::endl;
-    exit(1);
-}
-
-bool cmdOptionExists(std::vector<std::string> tokens, std::string option, std::string fullOption = "")
-{
-    bool optionExists = std::find(tokens.begin() + 1, tokens.end(), option) != tokens.end();
+    bool optionExists = std::find(tokens.begin(), tokens.end(), option) != tokens.end();
     bool fullOptionExists = false;
     if (fullOption != "")
     {
-        fullOptionExists = std::find(tokens.begin() + 1, tokens.end(), fullOption) != tokens.end();
+        fullOptionExists = std::find(tokens.begin(), tokens.end(), fullOption) != tokens.end();
     }
     return optionExists || fullOptionExists;
 }
 
-std::string getFilename(std::vector<std::string> tokens)
-{
-    auto result = std::find_if(tokens.begin() + 1, tokens.end(),
-        [](std::string i) { return i.ends_with(".бр") || i.ends_with(".барвінок"); });
-
-    if (result == tokens.end())
-    {
-        std::cout << usage(tokens[0]);
-        exit(0);
-    }
-
-    return *result;
-}
+#define COMPARE_OPTION(token, option, fullOption) \
+    token == option || token == fullOption
 
 #ifdef _WIN32
 int wmain(int argc, wchar_t* w_argv[])
@@ -113,7 +86,7 @@ int wmain(int argc, wchar_t* w_argv[])
 	char** argv = new char* [argc];
 	for (int i = 0; i < argc; ++i)
 	{
-		auto utf8String = convertWideToUtf8(std::wstring(w_argv[i]));
+		auto utf8String = convertWideToUtf8(wstring(w_argv[i]));
 		argv[i] = new char[utf8String.size()];
 		strcpy(argv[i], utf8String.c_str());
 	}
@@ -122,7 +95,7 @@ int wmain(int argc, wchar_t* w_argv[])
 int main(int argc, char* argv[])
 {
 #endif
-    auto programName = std::string(argv[0]);
+    auto programName = string(argv[0]);
     programName = programName.substr(programName.find_last_of("/\\") + 1);
     if (argc == 1)
     {
@@ -130,30 +103,50 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    std::vector<std::string> tokens;
-    for (int i = 0; i < argc; ++i)
+    vector<string> tokens(argv + 1, argv + argc);
+    vector<string> argsForInterpreter; // Аргументи для інтерпретатора
+    vector<string> argsForProgram; // Аргументи для програми запущеної інтерпретатором
+    for (int i = 0; i < (int)tokens.size(); ++i)
     {
-        tokens.push_back(std::string(argv[i]));
+        auto token = tokens[i];
+        if (COMPARE_OPTION(token, "-д", "--допомога"))
+        {
+            std::cout << usage(programName);
+            return 0;
+        }
+#ifdef DEBUG
+        else if (COMPARE_OPTION(token, "-т", "--токени")
+                || COMPARE_OPTION(token, "-а", "--асемблер"))
+        {
+            continue;
+        }
+#endif
+        else if ((token.ends_with(".бр") || token.ends_with(".барвінок")) && !token.starts_with("-"))
+        {
+            for (int j = 0; j < (int)tokens.size(); ++j)
+            {
+                if (j <= i) argsForInterpreter.push_back(tokens[j]);
+                else argsForProgram.push_back(tokens[j]);
+            }
+            break;
+        }
+        else
+        {
+            std::cout << "Невідомий аргумент: \"" << token << "\"" << std::endl;
+            return 0;
+        }
     }
 
-	if (cmdOptionExists(tokens, "-д", "--допомога"))
-	{
-		std::cout << usage(programName);
-		return 0;
-	}
-
-    auto filename = getFilename(tokens);
-
-	auto code = readCode(filename);
+	auto code = readCode(argsForInterpreter.back());
 	periwinkle::Periwinkle interpreter(code);
 
 #ifdef DEBUG
-	if (cmdOptionExists(tokens, "-т", "--токени"))
+	if (cmdOptionExists(argsForInterpreter, "-т", "--токени"))
 	{
 		interpreter.printTokens();
 		return 0;
 	}
-	if (cmdOptionExists(tokens, "-а", "--асемблер"))
+	if (cmdOptionExists(argsForInterpreter, "-а", "--асемблер"))
 	{
 		interpreter.printDisassemble();
 		return 0;
