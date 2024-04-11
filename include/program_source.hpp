ebc8fbcae865f15c7dc9f6f0ce2d3fd1d4c31543
romanfedyniak <fedynuak.roma@gmail.com>
2024-04-11 18:22:47 +0300

    ProgramSource клас

---
+++ include/program_source.hpp
@@ -0,0 +1,31 @@
+﻿#ifndef PROGRAM_SOURCE_HPP
+#define PROGRAM_SOURCE_HPP
+
+#include <string>
+#include <string_view>
+#include <filesystem>
+
+#include "exports.hpp"
+
+namespace periwinkle
+{
+    class API ProgramSource
+    {
+    private:
+        std::filesystem::path path;
+        std::string name;
+        std::string text;
+        bool _hasFile;
+
+    public:
+        std::string_view getText() const;
+        std::filesystem::path getPath() const;
+        std::string getFilename() const;
+        bool hasFile() const;
+
+        ProgramSource(const std::filesystem::path& path);
+        ProgramSource(const std::string& code, const std::string& name = "код");
+    };
+}
+
+#endif
---
+++ periwinkle/program_source.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "program_source.hpp"
+#include "utils.hpp"
+
+using namespace periwinkle;
+
+std::string_view ProgramSource::getText() const
+{
+    return text;
+}
+
+std::filesystem::path ProgramSource::getPath() const
+{
+    return path;
+}
+
+std::string ProgramSource::getFilename() const
+{
+    if (_hasFile) return path.filename().string();
+    return name;
+}
+
+bool ProgramSource::hasFile() const
+{
+    return _hasFile;
+};
+
+ProgramSource::ProgramSource(const std::filesystem::path& path)
+{
+    this->name = path.string();
+
+#ifdef _WIN32
+    std::ifstream filestream(utils::convertUtf8ToWide(path.string()));
+#else
+    std::ifstream filestream(path);
+#endif
+    std::stringstream ss;
+    if (filestream.is_open())
+    {
+        ss << filestream.rdbuf();
+        filestream.close();
+        this->text = ss.str();
+    }
+    else
+    {
+        std::cerr << "Неможливо відкрити файл \"" << path << "\"." << std::endl;
+        exit(1);
+    }
+
+    this->_hasFile = true;
+}
+
+ProgramSource::ProgramSource(const std::string& code, const std::string& name)
+{
+    this->name = name;
+    this->text = code;
+    this->_hasFile = false;
+}
